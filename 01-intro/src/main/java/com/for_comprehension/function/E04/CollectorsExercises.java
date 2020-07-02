e254fbaa97e70c8320e7d900e608b5b69e1d3a2e
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-07-02 09:31:18 +0200

    Collectors

---
+++ 01-intro/src/main/java/com/for_comprehension/function/E04/CollectorsExercises.java
@@ -0,0 +1,107 @@
+package com.for_comprehension.function.E04;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
+class CollectorsExercises {
+
+    /**
+     * Collect elements to a {@link List} instance
+     */
+    static Function<List<String>, List<String>> L1_toList() {
+        return list -> {
+            return null;
+        };
+    }
+
+    /**
+     * Collect elements to a {@link LinkedList} instance
+     */
+    static Function<List<String>, LinkedList<String>> L2_toLinkedList() {
+        return list -> {
+            return null;
+        };
+    }
+
+    /**
+     * Collect elements to a {@link List} wrapped in {@link Collections#unmodifiableList(List)} instance
+     */
+    static Function<List<String>, List<String>> L3_unmodifiable() {
+        return list -> {
+            return null;
+        };
+    }
+
+    /**
+     * Collect elements to a {@link Map} instance with uppercased elements as keys and their corresponding lengths as values
+     * and resolve potential collisions
+     */
+    static Function<List<String>, Map<String, Integer>> L4_toMap() {
+        return list -> {
+            return null;
+        };
+    }
+
+
+    /**
+     * Collect elements to a {@link TreeMap} instance with elements as keys and their corresponding lengths as values
+     * and resolve potential collisions by picking any of the strings
+     */
+    static Function<List<String>, Map<String, Integer>> L5_toTreeMap() {
+        return list -> {
+            return null;
+        };
+    }
+
+    /**
+     * Collect Map elements to a JSON String instance
+     * {@link Collectors#joining(CharSequence, CharSequence, CharSequence)}
+     */
+    static Function<Map<String, String>, String> L6_toJson() {
+        return input -> {
+            return null;
+        };
+    }
+
+
+    /**
+     * Group Strings of the same length
+     * {@link Collectors#groupingBy(Function)}
+     */
+    static Function<List<String>, Map<Integer, List<String>>> L7_groupStrings() {
+        return input -> {
+            return null;
+        };
+    }
+
+    /**
+     * Group Strings of the same length to a {@link TreeMap}
+     */
+    static Function<List<String>, TreeMap<Integer, List<String>>> L8_groupStrings() {
+        return input -> {
+            return null;
+        };
+    }
+
+    /**
+     * Group Strings of the same length into a comma-delimited String
+     * {@link Collectors#groupingBy(Function)}
+     */
+    static Function<List<String>, Map<Integer, String>> L9_groupStrings() {
+        return input -> {
+            return null;
+        };
+    }
+}
---
+++ 01-intro/src/test/java/com/for_comprehension/function/E04/CollectorsExercisesTest.java
@@ -0,0 +1,133 @@
+package com.for_comprehension.function.E04;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CollectorsExercisesTest {
+
+    @Test
+    public void l1_toList() {
+        // when
+        final List<String> result = CollectorsExercises.L1_toList().apply(asList("foo"));
+
+        // then
+        assertThat(result)
+          .isInstanceOf(List.class)
+          .containsExactly("foo");
+    }
+
+    @Test
+    public void l2_toLinkedList() {
+        // when
+        final List<String> result = CollectorsExercises.L2_toLinkedList().apply(asList("foo"));
+
+        // then
+        assertThat(result)
+          .isInstanceOf(LinkedList.class)
+          .containsExactly("foo");
+    }
+
+    @Test
+    public void l3_unmodifiable() {
+        // when
+        final List<String> result = CollectorsExercises.L3_unmodifiable().apply(singletonList("foo"));
+
+        // then
+        assertThatThrownBy(() -> result.add(""))
+          .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    public void l4_toMap() {
+
+        // when
+        final Map<String, Integer> result = CollectorsExercises.L4_toMap().apply(asList("42", "7") );
+
+        // then
+        assertThat(result)
+          .containsEntry("42", 2)
+          .containsEntry("7", 1);
+    }
+
+    @Test
+    public void l5_toTreeMap() {
+        // when
+        final Map<String, Integer> result = CollectorsExercises.L5_toTreeMap().apply(asList("42", "42"));
+
+        // then
+        assertThat(result)
+          .isInstanceOf(TreeMap.class)
+          .containsEntry("42", 2);
+    }
+
+    @Test
+    public void l6_toJson() {
+        // given
+        final Map<String, String> input = new HashMap<String, String>() {{
+            put("name", "Linus");
+            put("age", "42");
+        }};
+
+        // given
+        String result = CollectorsExercises.L6_toJson().apply(input);
+
+        // then
+        assertThat(result).isEqualTo("{\"name\":\"Linus\",\"age\":\"42\"}");
+    }
+
+    @Test
+    public void l7_groupStrings() {
+        // given
+        List<String> input = asList("Java", "Haskell", "Perl", "Scala");
+
+        // given
+        Map<Integer, List<String>> result = CollectorsExercises.L7_groupStrings().apply(input);
+
+        // then
+        assertThat(result)
+          .containsEntry(4, asList("Java", "Perl"))
+          .containsEntry(7, asList("Haskell"))
+          .containsEntry(5, asList("Scala"));
+    }
+
+    @Test
+    public void l8_groupStrings() {
+        // given
+        List<String> input = asList("Java", "Haskell", "Perl", "Scala");
+
+        // given
+        Map<Integer, List<String>> result = CollectorsExercises.L8_groupStrings().apply(input);
+
+        // then
+        assertThat(result)
+          .isInstanceOf(TreeMap.class)
+          .containsEntry(4, asList("Java", "Perl"))
+          .containsEntry(7, asList("Haskell"))
+          .containsEntry(5, asList("Scala"));
+    }
+
+    @Test
+    public void l9_groupStrings() {
+        // given
+        List<String> input = asList("Java", "Haskell", "Perl", "Scala");
+
+        // given
+        Map<Integer, String> result = CollectorsExercises.L9_groupStrings().apply(input);
+
+        // then
+        assertThat(result)
+          .containsEntry(4, "Java,Perl")
+          .containsEntry(7, "Haskell")
+          .containsEntry(5, "Scala");
+    }
+}
